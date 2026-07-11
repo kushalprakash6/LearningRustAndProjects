ba475df1f12b672590cb86a78b1a41add8fde386
kushalprakash6 <kushal.prakash@yahoo.in>
2026-07-11 20:32:17 +0200

    Trying variables

---
+++ variables_and_mutability/src/main.rs
@@ -0,0 +1,14 @@
+/*
+Copyright 2026
+Author : Kushal Prakash
+Last updated : 11/07/2026
+*/
+fn main() {
+    println!("Hello, world!");
+
+    let apples = 100;
+    let oranges = 20+40;
+    let fruits = apples + oranges;
+    println!("My garden has {apples} apples and {oranges} oranges, for a total of {fruits} fruits.");
+    println!("My garden has {1} apples and {0} oranges, for a total of {2} fruits.", oranges, apples, fruits);
+}
