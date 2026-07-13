dd60a75e5f89914a84c4297d79c8611bb2d0df50
kushalprakash6 <kushal.prakash@yahoo.in>
2026-07-13 17:02:21 +0200

    started with control flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,18 @@
+/*
+Copyright 2026
+Author : Kushal Prakash
+Last updated : 13/07/2026
+*/
+fn main() {
+
+    let num = 10;
+
+    if num == 1 {
+        println!("Number is 1");
+
+    }
+    if num != 1 {
+        println!("Number is not 1");
+    }
+
+}
