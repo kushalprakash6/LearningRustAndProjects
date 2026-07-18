80de745706791176588c7954f0318796348773ad
kushalprakash6 <kushal.prakash@yahoo.in>
2026-07-18 16:22:58 +0200

    finished structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,8 +5,93 @@
         is_hot: bool,
     }
 
+    struct Movies {
+        title: String,
+        release_year: u32,
+        duration_min: u32,
+    }
+
+impl Movies {
+    // fn display_movie_info (self) {
+    //     println!("Title is {}", self.title);
+    //     println!("release year is {}", self.release_year);
+    //     println!("duration is {}", self.duration_min);
+
+    // }
+
+    // fn display_movie_info (self: &Self) {
+    //     println!("Title is {}", self.title);
+    //     println!("release year is {}", self.release_year);
+    //     println!("duration is {}", self.duration_min);
+
+    // }
+
+    fn new(title: String, release_year: u32, duration_min: u32) -> Self {
+        Movies {
+            title,
+            release_year,
+            duration_min,
+        }
+    }
+
+    fn display_movie_info (&self) {
+        println!("Title is {}", self.title);
+        println!("release year is {}", self.release_year);
+        println!("duration is {}", self.duration_min);
+        println!("Years since release is {}", self.years_since_release());
+
+    }
+
+    // fn double_length (mut self) {
+    //     self.duration_min = self.duration_min*2;
+    // }
+
+    // fn double_length (self: &mut Movies) {
+    //     self.duration_min = self.duration_min*2;
+    // }
+
+    fn double_length (&mut self) {
+        self.duration_min = self.duration_min*2;
+    }
+
+    fn is_longer_than (&self, other:&Self) -> bool {
+        self.duration_min > other.duration_min
+    }
+
+    fn years_since_release(&self) -> u32 {
+        2026 - self.release_year
+    }
+}
+
 
 fn main() {
+
+    let film = Movies {
+        title: String::from("Upendra"),
+        release_year: 1999,
+        duration_min: 150,
+    };
+
+    let film_2 = Movies {
+        title: String::from("A"),
+        release_year: 2000,
+        duration_min: 149,
+    };
+
+    let film_3 = Movies::new(String::from("Om"), 1996, 160);
+
+
+
+    film.display_movie_info();
+
+    film_2.display_movie_info();
+
+    film_3.display_movie_info();
+
+    if film.is_longer_than(&film_2) {
+        println!("{} is longer than {}", film.title, film_2.title);
+    }
+
     struct Coffee {
         price: f64,
         name: String,
