7b5d616830f04c16c5ee5a30b44224230d9835fc
kushalprakash6 <kushal.prakash@yahoo.in>
2026-07-20 12:28:55 +0200

    Finished coding challenge 11

---
+++ coding_challenge_11/src/main.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+enum DigitalConsent {
+    AudioFile,
+    VideoFile,
+}
+
+#[derive(Debug)]
+struct ChatMessage<T> {
+    content: T,
+    time: String,
+}
+
+impl ChatMessage<DigitalConsent> {
+    fn consume_entertainment(&self) {
+        println!("{:?}", self.content)
+    }
+}
+
+impl<T> ChatMessage<T> {
+    fn retrieve_time(&self) -> String {
+        self.time.clone()
+
+    }
+}
+fn main() {
+
+    let ex1 = ChatMessage {
+        content: "String slice",
+        time: String::from("12:34"),
+    };
+    println!("{}",ex1.retrieve_time());
+
+    let ex2 = ChatMessage {
+        content: String::from("String slice"),
+        time: String::from("11:34"),
+    };
+    println!("{}",ex2.retrieve_time());
+
+    let ex3 = ChatMessage {
+        content: DigitalConsent::VideoFile,
+        time: String::from("14:23"),
+    };
+    println!("{}",ex3.retrieve_time());
+    ex3.consume_entertainment();
+
+}
